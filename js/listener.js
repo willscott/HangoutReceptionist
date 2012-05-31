041cbc7a707e28d9daac5fdc7e7b0fcf55c9c441
Will Scott <willscott@gmail.com>
2012-05-30 20:48:25 -0700

    Automatically opens hangout window when invited.

--- js/listener.js
+++ js/listener.js
@@ -9,6 +9,67 @@
     var link = notificationLinkQuery.iterateNext();
     return link;
   };
+
+  var findStreamContainer = function() {
+    var streamQuery = document.evaluate(
+        '//div[@guidedhelpid="streamcontent"]',
+        document.body,
+        null,
+        XPathResult.ANY_TYPE,
+        null);
+    var div = streamQuery.iterateNext();
+    return div;
+  }; 
+   
+  var findHangoutButton = function() {
+    var hangoutButtonQuery = document.evaluate(
+        '//div[@guidedhelpid="hangouts"]//div[@role="button"]',
+        document.body,
+        null,
+        XPathResult.ANY_TYPE,
+        null);
+    var button;
+    while(button = hangoutButtonQuery.iterateNext()) {
+      if (!button) {break;}
+      if (button.innerText.indexOf("Hang out") > -1) {
+        return button;
+      }
+    }
+  };
+  
+  var quietPhase = 0;
+  var inQuietPhase = function() {
+    return (new Date() - quietPhase) / 1000 < 120; // 2 minutes, for calming.
+  };
+  var startQuietPhase = function() {
+    quietPhase = new Date();
+  };
+  
+  var makeMouse = function(what) {
+    var evt = document.createEvent("MouseEvents");
+    evt.initMouseEvent("mouse" + what, true, true, window, 0,0,0,0,0,false,false,false,false,0,null);
+    return evt;
+  };
+  
+  var maybeTakeAction = function() {
+    var button = findHangoutButton();
+    if (button && !inQuietPhase()) {
+      console.log("clicking");
+      window.setTimeout(function() {
+        var over = makeMouse("over");
+        button.dispatchEvent(over);
+      }, 1);
+      window.setTimeout(function() {
+        var down = makeMouse("down");
+        button.dispatchEvent(down);
+      }, 100);
+      window.setTimeout(function() {
+        var up = makeMouse("up");
+        button.dispatchEvent(up);
+      }, 200);
+      startQuietPhase();
+    }
+  }
   
   var modificationListener = function(element, e) {
     var count = 0;
@@ -16,12 +77,27 @@
       count = parseInt(element.innerText.trim());
     } catch (e) {}
     if (count) {
-      // Find a hangout.
+      element.click();
+      maybeTakeAction();
+    }
+  };
+  var postListener = function(element, e) {
+    var postQuery = document.evaluate(
+        '//div[starts-with(@id,"update-")]',
+        element,
+        null,
+        XPathResult.ANY_TYPE,
+        null);
+    var firstPost = postQuery.iterateNext();
+    if (firstPost) {
+      if (firstPost.innerText.indexOf("Hang out") > -1) {
+        window.setTimeout(maybeTakeAction, 2000); //let the post animate in.
+      }
     }
   };
 
-  var listenToElement = function(element) {
-    var boundListener = modificationListener.bind(this, element);
+  var listenToElement = function(element, handler) {
+    var boundListener = handler.bind(this, element);
     element.addEventListener('DOMSubtreeModified', boundListener, false);
     element.addEventListener('DOMNodeRemoved', function(e) {
       element.removeEventListener('DOMSubtreeModified', boundListener, false);
@@ -29,7 +105,12 @@
   };
 
   addEventListener('load', function() {
-    var link = findNotificationButton();
-    listenToElement(link);
+    var stream = findStreamContainer();
+    if (stream) {
+      listenToElement(stream, postListener);      
+    } else {
+      var link = findNotificationButton();
+      listenToElement(link, modificationListener);
+    }
   }, false);
 })();
