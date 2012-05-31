11aee1b443c451171d97f8e71fa1e1fe2a3552a8
Will Scott <willscott@gmail.com>
2012-05-31 13:39:25 -0700

    now clicks the join button

---
+++ js/hangout.js
@@ -0,0 +1,57 @@
+(function() {
+  var findJoinButton = function() {
+    var buttonQuery = document.evaluate(
+        '//div[@role="button"]',
+        document.body,
+        null,
+        XPathResult.ANY_TYPE,
+        null);
+    var link;
+    while (link = buttonQuery.iterateNext()) {
+      if (link.innerText.indexOf("Join") > -1) {
+        return link;
+      }
+    }
+    return null;
+  };
+
+  var quietPhase = 0;
+  var inQuietPhase = function() {
+    return (new Date() - quietPhase) / 1000 < 120; // 2 minutes, for calming.
+  };
+  var startQuietPhase = function() {
+    quietPhase = new Date();
+  };
+  
+  var makeMouse = function(what) {
+    var evt = document.createEvent("MouseEvents");
+    evt.initMouseEvent("mouse" + what, true, true, window, 0,0,0,0,0,false,false,false,false,0,null);
+    return evt;
+  };
+  
+  var maybeTakeAction = function(button) {
+    if (button && !inQuietPhase()) {
+      console.log("clicking");
+      window.setTimeout(function() {
+        var over = makeMouse("over");
+        button.dispatchEvent(over);
+      }, 1);
+      window.setTimeout(function() {
+        var down = makeMouse("down");
+        button.dispatchEvent(down);
+      }, 100);
+      window.setTimeout(function() {
+        var up = makeMouse("up");
+        button.dispatchEvent(up);
+      }, 200);
+      startQuietPhase();
+    }
+  }
+
+  window.setTimeout(function() {
+    var join = findJoinButton();
+    if (join) {
+      maybeTakeAction(join);
+    }
+  }, 5000);
+})();
